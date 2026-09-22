ac6959754721bde1497a9fe2cea802289cda81a3
KED <matheuslite0333@gmail.com>
2026-09-22 14:39:38 -0300

    fix: garantir JSON no endpoint de relatório de brancas

--- api/brancas/relatorio.ts
+++ api/brancas/relatorio.ts
@@ -1,2 +1,10 @@
-import relatorioHandler from '../_brancas/relatorioV2';
-export default relatorioHandler;
+import { runSafeBrancasHandler } from '../_lib/safe-brancas-entry';
+
+export default async function handler(req: any, res: any) {
+  return runSafeBrancasHandler(
+    req,
+    res,
+    'relatorio',
+    () => import('../_brancas/relatorioV2')
+  );
+}
